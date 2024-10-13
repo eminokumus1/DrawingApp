9592baffe14630947826f624df788c4a982700ab
eminokumus1 <eminokumus1@gmail.com>
2024-10-13 17:51:38 +0300

    Color pallet added

--- app/src/main/java/com/eminokumus/drawingapp/MainActivity.kt
+++ app/src/main/java/com/eminokumus/drawingapp/MainActivity.kt
@@ -3,6 +3,11 @@ package com.eminokumus.drawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import androidx.core.view.marginTop
+import androidx.core.view.setPadding
 import com.eminokumus.drawingapp.databinding.ActivityMainBinding
 import com.eminokumus.drawingapp.databinding.DialogBrushSizeBinding
 
@@ -13,12 +18,16 @@ private const val largeBrushSize = 30f
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private var currentColorImageButton: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.drawingView.setSizeForBrush(20.toFloat())
+        binding.drawingView.setSizeForBrush(mediumBrushSize)
+        currentColorImageButton = binding.paintColorsLinearLayout[1] as ImageButton
+        setPalletColorToPressed(currentColorImageButton!!)
 
         setBrushImageButtonOnClickListener()
     }
@@ -34,18 +43,7 @@ class MainActivity : AppCompatActivity() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(brushBinding.root)
         brushDialog.setTitle("Brush size: ")
-        brushBinding.smallBrushImageButton.setOnClickListener {
-            binding.drawingView.setSizeForBrush(smallBrushSize)
-            brushDialog.dismiss()
-        }
-        brushBinding.mediumBrushImageButton.setOnClickListener {
-            binding.drawingView.setSizeForBrush(mediumBrushSize)
-            brushDialog.dismiss()
-        }
-        brushBinding.largeBrushImageButton.setOnClickListener {
-            binding.drawingView.setSizeForBrush(largeBrushSize)
-            brushDialog.dismiss()
-        }
+        setBrushDialogButtonsOnClickListeners(brushBinding, brushDialog)
         brushDialog.show()
     }
 
@@ -62,5 +60,16 @@ class MainActivity : AppCompatActivity() {
             binding.drawingView.setSizeForBrush(largeBrushSize)
             brushDialog.dismiss()
         }
+    }
+
+    private fun setPalletColorToPressed(colorButton: ImageButton){
+        colorButton.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_selected)
+        )
+    }
+    private fun setPalletColorToNormal(colorButton: ImageButton){
+        colorButton.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+        )
     }
 }
